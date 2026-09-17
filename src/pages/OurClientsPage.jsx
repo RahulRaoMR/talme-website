a44223d58d62de3b71f30d036a1098548fc66d06
RahulRaoMR <your-real-email@gmail.com>
2026-09-17 16:10:40 +0530

    Remove selected client cards

--- src/pages/OurClientsPage.jsx
+++ src/pages/OurClientsPage.jsx
@@ -1,29 +1,19 @@
 import "./OurClientsPage.css";
 
 const clientLogos = [
-  { name: "Tata Technologies", logo: "https://logo.clearbit.com/tatatechnologies.com" },
   { name: "L&T Technology Services", logo: "https://logo.clearbit.com/ltts.com" },
   { name: "PAMP Technologies", logo: "https://logo.clearbit.com/pamp.com" },
-  { name: "Wipro", logo: "https://logo.clearbit.com/wipro.com" },
   { name: "VVDN Technologies", logo: "https://logo.clearbit.com/vvdntech.com" },
   { name: "HL Klemove", logo: "https://logo.clearbit.com/hlklemove.com" },
-  { name: "Cyient", logo: "https://logo.clearbit.com/cyient.com" },
-  { name: "Tech Mahindra", logo: "https://logo.clearbit.com/techmahindra.com" },
   { name: "Yokogawa", logo: "https://logo.clearbit.com/yokogawa.com" },
   { name: "SLK", logo: "https://logo.clearbit.com/slksoftware.com" },
   { name: "Edwards", logo: "https://logo.clearbit.com/edwardsvacuum.com" },
   { name: "MindIT", logo: "https://logo.clearbit.com/mindit.io" },
-  { name: "Quest Global", logo: "https://logo.clearbit.com/quest-global.com" },
   { name: "Leadsoc", logo: "https://logo.clearbit.com/leadsoc.com" },
   { name: "Enparadigm", logo: "https://logo.clearbit.com/enparadigm.com" },
-  { name: "DiligenceVault", logo: "https://logo.clearbit.com/diligencevault.com" },
   { name: "ThoughtFocus", logo: "https://logo.clearbit.com/thoughtfocus.com" },
   { name: "Quinnel Soft", logo: "https://logo.clearbit.com/quinnelsoft.com" },
-  { name: "NTT DATA", logo: "https://logo.clearbit.com/nttdata.com" },
-  { name: "KPIT", logo: "https://logo.clearbit.com/kpit.com" },
-  { name: "Turiya", logo: "https://logo.clearbit.com/turiyatech.com" },
   { name: "Aquimo", logo: "https://logo.clearbit.com/aquimo.com" },
-  { name: "Radiant", logo: "https://logo.clearbit.com/radiant.com" },
 ];
 
 function OurClientsPage() {
